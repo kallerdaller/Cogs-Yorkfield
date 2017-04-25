298d7dc6bb2b88ebf1a3d92d0f3268132cbfb41e
kallerdaller <darley0alex1@gmail.com>
2017-04-25 20:42:30 +0100

    Update Meh.py
    Delete message Attempt#1

--- Meh/Meh.py
+++ Meh/Meh.py
@@ -2,16 +2,17 @@ import discord
 from discord.ext import commands
 
 class Mycog:
-    """My custom cog that does stuff!"""
+    """Tells a user that you said meh"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def meh(self, user : discord.Member):
-        """This does stuff!"""
+    async def meh(self, user : discord.Member, message):
+        """Tags a person and tells them meh"""
 
         #Your code will go here
+        delete_message(message)
         await self.bot.say("Meh " + user.mention)
 
 def setup(bot):
