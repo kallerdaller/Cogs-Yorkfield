1b9828be750a487cdca15b235642efa0d813d8f3
kallerdaller <darley0alex1@gmail.com>
2017-04-30 10:45:21 +0100

    Fixed syntax

--- russianroulette/russianroulette.py
+++ russianroulette/russianroulette.py
@@ -2,13 +2,17 @@ import discord
 from discord.ext import commands
 
 class Mycog:
-    ""Russian Roulette"""
+    """Russian Roulette"""
+    
     def __init__(self, bot):
         self.bot = bot
+        
     @commands.command(pass_contect=True, aliases=["RR", "Russian"])
     async def RussianRoulette(self):
         """This does stuff!"""
+        
         #Your code will go here
         await self.bot.say("I can do stuff!")
+        
 def setup(bot):
     bot.add_cog(Mycog(bot))
