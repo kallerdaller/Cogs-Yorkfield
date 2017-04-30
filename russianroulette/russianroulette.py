7077da6fd15a65d98ea1724975ee2db40aeccc9b
kallerdaller <darley0alex1@gmail.com>
2017-04-30 13:21:42 +0100

    Place bet

--- russianroulette/russianroulette.py
+++ russianroulette/russianroulette.py
@@ -35,7 +35,14 @@ class Russianroulette:
             
             
 async def betAmount():
+    await self.bot.say("How much would you like to put on the line: $")
     bet = await self.bot.wait_for_message(timeout=60, author=user, check=check)
+    if bet is int:
+        bet = int(float(bet))
+        await self.bot.say("Bet placed at $" + string(bet)
+    else:
+         await self.bot.say("You must enter a number")
+         betAmount()
 
 def check_folders():
     if not os.path.exists("data/russianroulette"): 
