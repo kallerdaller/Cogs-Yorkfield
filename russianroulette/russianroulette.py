81ed3907d515ee492c0b72f30a82a0057a6a13e3
kallerdaller <darley0alex1@gmail.com>
2017-04-30 16:38:23 +0100

    Save json data

--- russianroulette/russianroulette.py
+++ russianroulette/russianroulette.py
@@ -57,6 +57,7 @@ class Russianroulette:
                 if bank.get_balance(user) > bet:
                     self.json_data["System"]["Bet"] = bet
                     self.json_data["System"]["Status"] = "Waiting"
+                    dataIO.save_json(f, json_data)
                     await self.bot.say("Bet placed at $" + str(bet))
                 else:
                     await self.bot.say("You don't have enough to place a bet of $" + str(bet) + " You only have $" + str(bank.get_balance(user)))
