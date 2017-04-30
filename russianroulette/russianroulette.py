077058a8a18e6174cf7ea31f168be8e92970fe1c
kallerdaller <darley0alex1@gmail.com>
2017-04-30 22:42:11 +0100

    Fix KeyError

--- russianroulette/russianroulette.py
+++ russianroulette/russianroulette.py
@@ -139,7 +139,7 @@ class Russianroulette:
             if self.json_data["Players"][str(b)] == "":
                 b += 1
             else:
-                winner = self.json_data["Players"][str(a)]
+                winner = self.json_data["Players"][str(b)]
         deposit_credits(discord.utils.get(ctx.message.server.members, id=winner), self.json_data["System"]["Bet"] * totalPlayers)
         await self.bot.say("Congrats " + discord.utils.get(ctx.message.server.members, id=winner).mention + " on winning $" + self.json_data["System"]["Bet"] * totalPlayers)
             
