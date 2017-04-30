26b638475c29041802134abc64f0d154af52c7de
kallerdaller <darley0alex1@gmail.com>
2017-04-30 13:31:17 +0100

    Fixed syntax fixed indent

--- russianroulette/russianroulette.py
+++ russianroulette/russianroulette.py
@@ -39,7 +39,7 @@ async def betAmount():
     bet = await self.bot.wait_for_message(timeout=60, author=user, check=check)
     if bet is int:
         bet = int(float(bet))
-        await self.bot.say("Bet placed at $" + string(bet)
+        await self.bot.say("Bet placed at $" + string(bet))
     else:
         await self.bot.say("You must enter a number")
         betAmount()
