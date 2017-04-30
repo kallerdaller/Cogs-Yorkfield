11b16154f4878f1435cb2ae98c6a9936c2914084
kallerdaller <darley0alex1@gmail.com>
2017-04-30 10:42:34 +0100

    Boiler plater + aliases

---
+++ russianroulette/russianroulette.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Mycog:
+    ""Russian Roulette"""
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command(pass_contect=True, aliases=["RR", "Russian"])
+    async def RussianRoulette(self):
+        """This does stuff!"""
+        #Your code will go here
+        await self.bot.say("I can do stuff!")
+def setup(bot):
+    bot.add_cog(Mycog(bot))
