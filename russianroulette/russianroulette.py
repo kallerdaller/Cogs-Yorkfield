e9c50b1a35b7bce6ae107d28e07fab04ba052c51
kallerdaller <darley0alex1@gmail.com>
2017-04-30 14:17:13 +0100

    Fix error

--- russianroulette/russianroulette.py
+++ russianroulette/russianroulette.py
@@ -39,6 +39,9 @@ class Russianroulette:
     async def betAmount(self, user):
         await self.bot.say("How much would you like to put on the line: $")
         bet = await self.bot.wait_for_message(timeout=60, author=user)
+        if bet is str:
+            await self.bot.say("You didn't enter anything")
+            return
         bet = int(float(bet))
         if bet is int:
             await self.bot.say("Bet placed at $" + string(bet))
