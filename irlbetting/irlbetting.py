23eb8e2089412b8b46d336af8a7d712352f9718b
kallerdaller <darley0alex1@gmail.com>
2017-05-01 16:14:14 +0100

    Month | Day | Hour | createevent not betevent | Out come need atleast 2

--- irlbetting/irlbetting.py
+++ irlbetting/irlbetting.py
@@ -16,20 +16,20 @@ class EventBets:
         self.file_path = "data/irlbetting/irlbetting.json"
         self.json_data = dataIO.load_json(self.file_path) 
 
-    @commands.command(pass_context=True, aliases=["be"])
+    @commands.command(pass_context=True, aliases=["ce"])
     @checks.admin_or_permissions(manage_server=True)
     @client.event
-    async def betevent(self, ctx):
+    async def createevent(self, ctx):
         """Create event"""
 
         #Your code will go here
         user = ctx.message.author
         await self.bot.say("What is the name of the event?")
         eventname = await self.bot.wait_for_message(timeout=30, author=user)
-        eventname = str(eventname.content)
         if eventname is None:
             await self.bot.say("You didn't enter anything. Event cancelled")
             return
+        eventname = str(eventname.content)
         await self.bot.say("Event name: \n" + eventname)
         await self.bot.say("What are the possible outcomes of the event? You must have atleast 2. To stop inputting outcomes simply type `.`")
         a = 0
@@ -38,16 +38,18 @@ class EventBets:
         while not message == ".":
             await self.bot.say(str(a+1) + ": ")
             outcome.append(await self.bot.wait_for_message(timeout=30, author=user))
-            outcome[a] = str(outcome[a].content)
             if outcome[a] is None:
                 if a == 3:
                     message = "."
                 else:
                     await self.bot.say("You didn't enter anything. Event cancelled")
                     return
+            outcome[a] = str(outcome[a].content)
             else:
                 message = outcome[a]
             a += 1
+        if len(outcome) < 2:
+            return
         await self.bot.say("Outcomes: ")
         i = 0
         while i < a-1:
@@ -55,15 +57,47 @@ class EventBets:
             i += 1
         await self.bot.say("What should the payout multiplier be?")
         payoutM = await self.bot.wait_for_message(timeout=30, author=user)
+        if payoutM is None:
+            await self.bot.say("You didn't enter anything. Event cancelled")
+            return
         try:
             payoutM = int(payoutM.content)
         except ValueError:
             await self.bot.say("You must enter a number. Event cancelled")
-        if payoutM is None:
+            return
+        await self.bot.say("The multiplier has been set to: " + str(payoutM))
+        await self.bot.say("In what month does the event happen? (Give as a number)")
+        month = await self.bot.wait_for_message(timeout = 30, author = user)
+        if month is None:
             await self.bot.say("You didn't enter anything. Event cancelled")
+            return
+        try:
+            month = int(month.contents)
+        except ValueError:
+            await self.bot.say("You have to enter a number. Event cancelled")
+            return
+        await self.bot.say("What day of the month does the event happen? (Must be a number)")
+        day = await self.bot.wait_for_message(timeout=30, author = user)
+        if day is None:
+            await self.bot.say("You didn't enter anything. Event cancelled")
+            return
+        try:
+            day = int(day.content)
+        except ValueError:
+            await self.bot.say("You have to enter a number. Event cancelled")
+            return 
+        await self.bot.say("What hour of the day does the event happen? (Must be in 24 hour format IE: 9PM is 21)")
+        hour = await self.bot.wait_for_message(timeout=30, author = user)
+        if hour is None:
+            await self.bot.say("You didn't enter anything. Event cancelled")
+            return
+        try:
+            hour = int(hour.content)
+        except ValueError:
+            await self.bot.say("You have to enter a number. Event cancelled")
+            return
+        await self.bot.say("The event happens at " + str(hour) + ":00 on " + str(month) + "/" + str(day))
         
-            
-
 def check_folders():
     if not os.path.exists("data/irlbetting"): 
         print("Creating data/irlbetting floder...")  
