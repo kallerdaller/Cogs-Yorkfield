3ea368647c73e75c3ebca4b818fc17b423f40aa1
kallerdaller <darley0alex1@gmail.com>
2017-05-01 14:10:13 +0100

    Debug

--- irlbetting/irlbetting.py
+++ irlbetting/irlbetting.py
@@ -9,15 +9,14 @@ class EventBets:
         self.bot = bot
 
     @commands.command(pass_context=True, aliases=["be"])
+    @checks.admin_or_permissions(manage_server=True)
     async def betevent(self, ctx):
         """Create event"""
 
         #Your code will go here
         user = ctx.message.author
         await self.bot.say("Event name?")
-        print(user.roles.id)
 
 def setup(bot):
     n = EventBets(bot)
     bot.add_cog(n)
-#308553548566757378
