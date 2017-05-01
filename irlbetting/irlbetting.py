35fbdc6e3d342ed6944046cafcd2816d62a661aa
kallerdaller <darley0alex1@gmail.com>
2017-05-01 12:50:20 +0100

    Boiler Plate

--- irlbetting/irlbetting.py
+++ irlbetting/irlbetting.py
@@ -1,1 +1,19 @@
+import discord
+from discord.ext import commands
 
+class Mycog:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context = True, aliases=["be"])
+    async def betevent(self):
+        """Create event"""
+
+        #Your code will go here
+        await self.bot.say("Event name?")
+
+def setup(bot):
+    n = EventBets(bot)
+    bot.add_cog(n)
