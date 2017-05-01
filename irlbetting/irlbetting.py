2a96ddedc9319e260506857fc9035b4bdf1183a4
kallerdaller <darley0alex1@gmail.com>
2017-05-01 13:17:29 +0100

    Find user roles

--- irlbetting/irlbetting.py
+++ irlbetting/irlbetting.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.permissions import Permissions
 
 class EventBets:
     """Create an event which people can bet on"""
@@ -12,8 +13,11 @@ class EventBets:
         """Create event"""
 
         #Your code will go here
+        user = ctx.message.author
         await self.bot.say("Event name?")
+        print(user.roles)
 
 def setup(bot):
     n = EventBets(bot)
     bot.add_cog(n)
+#308553548566757378
