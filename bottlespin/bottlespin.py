92663add1710048aaa31cf3a4070d5cf8d0415b1
kallerdaller <darley0alex1@gmail.com>
2017-07-03 22:10:21 +0100

    Update bottlespin.py

--- bottlespin/bottlespin.py
+++ bottlespin/bottlespin.py
@@ -15,6 +15,7 @@ class Bottlespin:
         """Spin the bottle"""
 
         roles = [ctx.message.server.roles]
+        role = ctx.message.role
         if role in roles:
             await self.bot.say(str(role))
         await self.bot.say(str(roles))
