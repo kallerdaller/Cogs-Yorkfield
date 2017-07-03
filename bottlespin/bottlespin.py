491544a7aa4394c98ff763946d614a7faeb6f09f
kallerdaller <darley0alex1@gmail.com>
2017-07-03 21:10:28 +0100

    Update bottlespin.py

--- bottlespin/bottlespin.py
+++ bottlespin/bottlespin.py
@@ -14,7 +14,7 @@ class Bottlespin:
     async def spin(self, ctx, role):
         """Spin the bottle"""
 
-        roles = [r.name for r in ctx.message.server.roles]
+        roles = [for r in ctx.message.server.roles]
         author = ctx.message.author
         server = ctx.message.server
 
