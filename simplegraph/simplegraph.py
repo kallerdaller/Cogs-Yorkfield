73d2950fc9d5713edf8796fd1e4155176046adca
kallerdaller <darley0alex1@gmail.com>
2017-07-04 19:39:43 +0100

    Update simplegraph.py

--- simplegraph/simplegraph.py
+++ simplegraph/simplegraph.py
@@ -88,7 +88,7 @@ def check_folder():  # Paddo is great
     if not path.exists("data/sgraph"):
         print("[SimpleGraph]Creating data/sgraph folder...")
         makedirs("data/sgraph")
-        dataIO.save_json("data/sgraph/settings.json", {})
+        fileIO("data/sgraph/settings.json", "save", {})
 
     if not path.exists("data/sgraph/temp"):
         print("[SimpleGraph]Creating data/sgraph/temp folder...")
